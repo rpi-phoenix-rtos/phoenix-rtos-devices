dd6a9fa66d3c50aa4066c8fed8702378981a1ef6
Witold Bołt <witold.bolt@jit.team>
2026-03-22 00:02:32 +0100

    usb: add pi4 xhci capability probe

--- usb/xhci/xhci.c
+++ usb/xhci/xhci.c
@@ -11,17 +11,136 @@
 
 #include <errno.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
 #include <hcd.h>
 
 
-static int xhci_init(hcd_t *hcd)
+#define XHCI_MAP_SIZE            0x10000u
+#define XHCI_REG_CAP_CAPLENGTH   0x00u
+#define XHCI_REG_CAP_HCIVERSION  0x02u
+#define XHCI_REG_CAP_HCSPARAMS1  0x04u
+#define XHCI_SUPPORTED_VERSION   0x0100u
+
+
+typedef struct {
+	void *mmio;
+	size_t mapSz;
+	uint8_t caplength;
+	uint16_t version;
+	uint32_t hcsparams1;
+} xhci_t;
+
+
+static inline uint8_t xhci_read8(xhci_t *xhci, uintptr_t off)
+{
+	volatile uint8_t *base = (volatile uint8_t *)xhci->mmio;
+
+	return *(base + off);
+}
+
+
+static inline uint16_t xhci_read16(xhci_t *xhci, uintptr_t off)
+{
+	volatile uint16_t *reg = (volatile uint16_t *)((volatile uint8_t *)xhci->mmio + off);
+
+	return *reg;
+}
+
+
+static inline uint32_t xhci_read32(xhci_t *xhci, uintptr_t off)
+{
+	volatile uint32_t *reg = (volatile uint32_t *)((volatile uint8_t *)xhci->mmio + off);
+
+	return *reg;
+}
+
+
+static void xhci_destroy(xhci_t *xhci)
+{
+	if (xhci == NULL) {
+		return;
+	}
+
+	if (xhci->mmio != MAP_FAILED) {
+		munmap(xhci->mmio, xhci->mapSz);
+	}
+
+	free(xhci);
+}
+
+
+static int xhci_map(hcd_t *hcd, xhci_t **xhcip)
+{
+	xhci_t *xhci;
+	off_t offs;
+
+	xhci = calloc(1, sizeof(*xhci));
+	if (xhci == NULL) {
+		return -ENOMEM;
+	}
+
+	xhci->mmio = MAP_FAILED;
+	xhci->mapSz = XHCI_MAP_SIZE;
+
+	offs = hcd->info->hcdaddr % _PAGE_SIZE;
+	xhci->mmio = mmap(NULL, xhci->mapSz, PROT_WRITE | PROT_READ, MAP_DEVICE | MAP_PHYSMEM | MAP_ANONYMOUS, -1, hcd->info->hcdaddr - offs);
+	if (xhci->mmio == MAP_FAILED) {
+		xhci_destroy(xhci);
+		return -ENOMEM;
+	}
+
+	hcd->base = (volatile uint32_t *)((uintptr_t)xhci->mmio + offs);
+	hcd->priv = xhci;
+	*xhcip = xhci;
+
+	return EOK;
+}
+
+
+static int xhci_capProbe(hcd_t *hcd, xhci_t *xhci)
 {
 	(void)hcd;
 
-	fprintf(stderr, "xhci: init not implemented\n");
+	xhci->caplength = xhci_read8(xhci, XHCI_REG_CAP_CAPLENGTH);
+	xhci->version = xhci_read16(xhci, XHCI_REG_CAP_HCIVERSION);
+	xhci->hcsparams1 = xhci_read32(xhci, XHCI_REG_CAP_HCSPARAMS1);
+
+	if ((xhci->caplength < 0x20u) || (xhci->caplength > 0xffu)) {
+		fprintf(stderr, "xhci: invalid caplength 0x%02x\n", xhci->caplength);
+		return -ENODEV;
+	}
+
+	if (xhci->version != XHCI_SUPPORTED_VERSION) {
+		fprintf(stderr, "xhci: unsupported version 0x%04x\n", xhci->version);
+		return -ENODEV;
+	}
 
 	return -ENOSYS;
+}
+
+
+static int xhci_init(hcd_t *hcd)
+{
+	xhci_t *xhci;
+	int err;
+
+	err = xhci_map(hcd, &xhci);
+	if (err < 0) {
+		return err;
+	}
+
+	err = xhci_capProbe(hcd, xhci);
+	if (err != 0) {
+		xhci_destroy(xhci);
+		hcd->priv = NULL;
+		hcd->base = NULL;
+	}
+
+	return err;
 }
 
 
